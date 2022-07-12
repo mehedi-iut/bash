dc1c197b3e033631780f607345130703a76e2d79
mehedi-iut <mahedihasan@iut-dhaka.edu>
2022-07-12 15:54:29 +0600

    make one line necessary packages installation

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@
 # update the system
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install ubuntu-restricted-extras -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common wget ncurses-dev make build-essential unzip gpg gnupg-agent lsb-release gnupg
+
 
 FILE=~/.bashrc
 if test -f "$FILE"; then
@@ -22,7 +24,7 @@ sudo apt install python3-pip -y
 sudo apt install golang -y
 
 # install edge web browser
-sudo apt install apt-transport-https ca-certificates curl software-properties-common wget -y
+# sudo apt install apt-transport-https ca-certificates curl software-properties-common wget -y
 sudo wget -O- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft-edge.gpg
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-edge.gpg] https://packages.microsoft.com/repos/edge stable main' | sudo tee /etc/apt/sources.list.d/microsoft-edge.list
 sudo apt update
@@ -35,12 +37,12 @@ sudo apt install gnome-tweaks -y
 
 # install vim from source
 # will be installed in /usr/local/bin/vim
-sudo apt install ncurses-dev -y
-sudo apt install make -y
-sudo apt install build-essential -y
+# sudo apt install ncurses-dev -y
+# sudo apt install make -y
+# sudo apt install build-essential -y
 
 wget https://github.com/vim/vim/archive/master.zip
-sudo apt install unzip -y
+# sudo apt install unzip -y
 unzip master.zip
 cd vim-master
 cd src/
@@ -52,13 +54,13 @@ cd
 
 
 # install visual studio code
-sudo apt-get install wget gpg -y
+# sudo apt-get install wget gpg -y
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 
-sudo apt install apt-transport-https -y
+# sudo apt install apt-transport-https -y
 sudo apt update -y
 sudo apt install code -y
 
@@ -88,12 +90,12 @@ sudo apt install vlc -y
 
 # install docker
 sudo apt-get update -y &&
-sudo apt-get install -y \
-apt-transport-https \
-ca-certificates \
-curl \
-gnupg-agent \
-software-properties-common &&
+# sudo apt-get install -y \
+# apt-transport-https \
+# ca-certificates \
+# curl \
+# gnupg-agent \
+# software-properties-common &&
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" &&
 sudo apt-get update -y &&
@@ -127,7 +129,7 @@ kubectl config view
 # install Azure CLI
 
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
+# sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
 curl -sL https://packages.microsoft.com/keys/microsoft.asc |
     gpg --dearmor |
     sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
@@ -138,4 +140,9 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
 
 sudo apt-get update -y
 sudo apt-get install -y azure-cli
+
+# install Terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y && sudo apt install terraform -y
 
