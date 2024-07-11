845fc6de9bccaa5bf1433647dfe508bc4a3ac41e
mehedi-iut <92790576+mehedi-iut@users.noreply.github.com>
2024-07-11 18:07:38 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -110,7 +110,7 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo usermod -aG docker ${USER}
 sudo chmod 666 /var/run/docker.sock
 
